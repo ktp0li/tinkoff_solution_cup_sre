82ee6f256f28b0318283c4b269436893e0746c5b
poli <polishka3004@gmail.com>
2023-04-16 10:14:40 +0300

    add logging

--- v1/main.py
+++ v1/main.py
@@ -1,7 +1,11 @@
 import docker
 import os
+import time
 client = docker.from_env()
 container = client.containers.run("puk",
         "pytest -v --video on /tmp && mv test-results/*/* /tmp",
         remove=True, volumes=[f'{os.getcwd()}/tests:/tmp'])
-print(container)
+while (logs := container.logs().decode("utf8") == ""):
+    time.sleep(2)
+
+print(logs)
