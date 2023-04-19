9fb145844fc20f25a63f95cebf395d2c52d3fb8c
poli <katunina0polina@gmail.com>
2023-04-19 22:06:10 +0300

    rm v2

--- v2/main.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask, request
-from flask.views import MethodView
-from flask_sqlalchemy import SQLAlchemy
-
-app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:qwertyui@51.81.26.19/postgres"
-db = SQLAlchemy(app)
