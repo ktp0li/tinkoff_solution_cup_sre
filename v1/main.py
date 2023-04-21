0f9a0c0da000d6c02e9dab5ab23186c50b8eb7cd
poli <katunina0polina@gmail.com>
2023-04-22 01:14:47 +0300

    finalize work with container

--- v1/main.py
+++ v1/main.py
@@ -7,7 +7,8 @@ container = client.containers.run("puk", volumes=[f'{os.getcwd()}/tests:/home'],
                                   detach=True, environment=[f"URL={url}"])
 for _ in range(999):
     if ("short test summary info" in
-       (logs := container.logs().decode("utf-8"))):
+        (logs := container.logs().decode("utf-8"))) or \
+         ("no tests run" in logs):
         break
     print(container.status)
     time.sleep(2)
