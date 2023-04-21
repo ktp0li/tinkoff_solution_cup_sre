e65054e81c7efe654c6694893863488a6994a17d
arelive <me@are.moe>
2023-04-22 02:59:44 +0300

    tabbed

--- v1/frontend/src/App.js
+++ v1/frontend/src/App.js
@@ -1,11 +1,13 @@
-import React, {useRef, useState} from 'react';
-import {FormLabel, TextField, Button, FormControl, List, ListItem} from '@mui/material';
+import React, {useState} from 'react';
+import {FormLabel, TextField, Button, FormControl, List, ListItem, Tabs, Tab, Box, TabPanel} from '@mui/material';
 
 import Dropzone from './components/Dropzone';
 
 
 const App = () => {
     const hostname = 'localhost:5000';
+
+    const [activeTab, setActiveTab] = useState('upload');
     const [username, setUsername] = useState('');
     const [websiteURL, setWebsiteURL] = useState('');
     const [testFiles, setTestFiles] = useState([]);
@@ -37,63 +39,90 @@ const App = () => {
     };
 
     return (<>
-        <FormControl
-            action='#'
-            method='POST'
-            encType='multipart/form-data'
-        >
-            <FormLabel> Пользователь </FormLabel>
-            <TextField
-                placeholder='username' type='text' size='medium' variant='outlined'
-                value={username}
-                onChange={event => setUsername(event.target.value)}
-            />
-            <FormLabel> URL тестируемой страницы </FormLabel>
-            <TextField
-                placeholder='http://example.com' type='url' size='medium' variant='outlined'
-                value={websiteURL}
-                onChange={event => setWebsiteURL(event.target.value)}
-            />
-            <Dropzone
-                onDrop={files => {
-                    for (let file of files) {
-                        const fr = new FileReader();
-                        fr.onload = event => setTestFiles([...testFiles, {meta: file, content: event.target.result}]);
-                        fr.readAsText(file);
-                    }
-                }}
+        <Box style={{
+            display: 'flex',
+            flexDirection: 'column',
+            background: '#EEEEFF',
+        }}>
+            <Tabs
+                variant='fullWidth'
+                value={activeTab}
+                onChange={(event, value) => setActiveTab(value)}
+                aria-label="wrapped label tabs example"
             >
-                <p>Drag 'n' drop some files here, or click to select files</p>
-            </Dropzone>
-            <List>
-                {testFiles.map(file => (
-                    <ListItem key={file.meta.path}>{file.meta.name}</ListItem>
-                ))}
-            </List>
-            <Button
-                type='reset' size='small' variant='outlined' color='error'
-                style={{marginBottom: '1rem'}}
-                onClick={(event) => {
-                    event.preventDefault();
-                    setTestFiles([]);
-                }}
-            >
-                Сбросить все тесты
-            </Button>
-            <Button
-                type='submit' size='medium' variant='contained' color='primary'
-                onClick={handleSubmit}
-            >
-                Запустить
-            </Button>
-        </FormControl>
-        <div>
-            <List>
-                {logs.map(log => (
-                    <ListItem key={testIndex++}>{log}</ListItem>
-                ))}
-            </List>
-        </div>
+                <Tab value='upload' label="Загрузить файлы" />
+                <Tab value='result' label='Получить результат' />
+                <Tab value='signup' label='Регистрация' />
+            </Tabs>
+        </Box>
+        <Box role='tabpanel'>
+            {activeTab === 'upload' && (<Box>
+                <FormControl
+                    action='#'
+                    method='POST'
+                    encType='multipart/form-data'
+                    style={{
+                        width: '100%',
+                        margin: '1rem 0',
+                    }}
+                >
+                    <FormLabel> Пользователь </FormLabel>
+                    <TextField
+                        placeholder='username' type='text' size='medium' variant='outlined'
+                        value={username}
+                        onChange={event => setUsername(event.target.value)}
+                    />
+                    <FormLabel> URL тестируемой страницы </FormLabel>
+                    <TextField
+                        placeholder='http://example.com' type='url' size='medium' variant='outlined'
+                        value={websiteURL}
+                        onChange={event => setWebsiteURL(event.target.value)}
+                    />
+                    <Dropzone
+                        onDrop={async files => {
+                            for (let file of files) {
+                                const fr = new FileReader();
+                                fr.onload = event => setTestFiles([...testFiles, {meta: file, content: event.target.result}]);
+                                fr.readAsText(file);
+                            }
+                        }}
+                    >
+                        <p>Drag 'n' drop some files here, or click to select files</p>
+                    </Dropzone>
+                    <List>
+                        {testFiles.map(file => (
+                            <ListItem key={file.meta.path}>{file.meta.name}</ListItem>
+                        ))}
+                    </List>
+                    <Button
+                        type='reset' size='small' variant='outlined' color='error'
+                        style={{marginBottom: '1rem'}}
+                        onClick={(event) => {
+                            event.preventDefault();
+                            setTestFiles([]);
+                        }}
+                    >
+                        Сбросить все тесты
+                    </Button>
+                    <Button
+                        type='submit' size='medium' variant='contained' color='primary'
+                        onClick={handleSubmit}
+                    >
+                        Запустить
+                    </Button>
+                </FormControl>
+                <Box>
+                    <List>
+                        {logs.map(log => (
+                            <ListItem key={testIndex++}>{log}</ListItem>
+                        ))}
+                    </List>
+                </Box>
+            </Box>)}
+            {activeTab === 'result' && (<Box>
+                
+            </Box>)}
+        </Box>
     </>);
 }
 
