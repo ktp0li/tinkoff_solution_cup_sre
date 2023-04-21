d7a472a77b75fdab446220c26bb1cecdad1aebee
poli <katunina0polina@gmail.com>
2023-04-21 11:43:08 +0300

    api.py: add websocket

--- v1/api.py
+++ v1/api.py
@@ -1,11 +1,28 @@
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, UploadFile, WebSocket
 import uvicorn
+import docker
+import os
+import time
 app = FastAPI()
 
 
 @app.post("/uploadfiles/")
 async def create_upload_files(files: list[UploadFile]):
     return {"filenames": [file.filename for file in files]}
+
+
+@app.websocket("/logs")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    client = docker.from_env()
+    container = client.containers.run("puk",
+                                      volumes=[f'{os.getcwd()}/tests:/tmp'],
+                                      detach=True)
+    while not ("short test summary info" in
+               (logs := container.logs().decode("utf-8"))):
+        time.sleep(2)
+    await websocket.send_text(logs)
+    await websocket.close()
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=5000, log_level="info")
--- v1/main.py
+++ v1/main.py
@@ -2,10 +2,14 @@ import docker
 import os
 import time
 client = docker.from_env()
-container = client.containers.run("puk",
-        "pytest -v --video on /tmp && mv test-results/*/* /tmp",
-        remove=True, volumes=[f'{os.getcwd()}/tests:/tmp'])
-while (logs := container.logs().decode("utf8") == ""):
+container = client.containers.run("puk", volumes=[f'{os.getcwd()}/tests:/tmp'],
+                                  detach=True)
+while not ("short test summary info" in
+           (logs := container.logs().decode("utf-8"))):
+    print(container.status)
+    logs = container.logs()
     time.sleep(2)
 
+print(container.status)
 print(logs)
+container.remove(force=True)
