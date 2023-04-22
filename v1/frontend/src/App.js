38951966acc846d83065149897af0f8d4548180a
arelive <me@are.moe>
2023-04-22 03:50:24 +0300

    enable to get result of test

--- v1/frontend/src/App.js
+++ v1/frontend/src/App.js
@@ -1,42 +1,15 @@
 import React, {useState} from 'react';
 import {FormLabel, TextField, Button, FormControl, List, ListItem, Tabs, Tab, Box, TabPanel} from '@mui/material';
 
-import Dropzone from './components/Dropzone';
+import UploadPanel from './panels/upload';
+import ResultPanel from './panels/result';
 
 
 const App = () => {
+
     const hostname = 'localhost:5000';
 
     const [activeTab, setActiveTab] = useState('upload');
-    const [username, setUsername] = useState('');
-    const [websiteURL, setWebsiteURL] = useState('');
-    const [testFiles, setTestFiles] = useState([]);
-    const [logs, setLogs] = useState([]);
-
-    let testIndex = 0;
-
-    const handleSubmit = event => {
-        event.preventDefault();
-        let socket = new WebSocket('ws://' + hostname + '/logs');
-
-        socket.onmessage = event => {
-            const log = (''+event.data)
-                .replace(/&/g, '&amp;')
-                .replace(/</g, '&lt;')
-                .replace(/>/g, '&gt;')
-                .replace(/"/g, '&quot;')
-                .replace(/'/g, '&#039;');
-            setLogs([...logs, log]);
-        };
-
-        socket.onopen = () => {
-            socket.send(JSON.stringify({
-                username: username,
-                url: websiteURL,
-                tests: testFiles
-            }))
-        }
-    };
 
     return (<>
         <Box style={{
@@ -56,72 +29,8 @@ const App = () => {
             </Tabs>
         </Box>
         <Box role='tabpanel'>
-            {activeTab === 'upload' && (<Box>
-                <FormControl
-                    action='#'
-                    method='POST'
-                    encType='multipart/form-data'
-                    style={{
-                        width: '100%',
-                        margin: '1rem 0',
-                    }}
-                >
-                    <FormLabel> Пользователь </FormLabel>
-                    <TextField
-                        placeholder='username' type='text' size='medium' variant='outlined'
-                        value={username}
-                        onChange={event => setUsername(event.target.value)}
-                    />
-                    <FormLabel> URL тестируемой страницы </FormLabel>
-                    <TextField
-                        placeholder='http://example.com' type='url' size='medium' variant='outlined'
-                        value={websiteURL}
-                        onChange={event => setWebsiteURL(event.target.value)}
-                    />
-                    <Dropzone
-                        onDrop={async files => {
-                            for (let file of files) {
-                                const fr = new FileReader();
-                                fr.onload = event => setTestFiles([...testFiles, {meta: file, content: event.target.result}]);
-                                fr.readAsText(file);
-                            }
-                        }}
-                    >
-                        <p>Drag 'n' drop some files here, or click to select files</p>
-                    </Dropzone>
-                    <List>
-                        {testFiles.map(file => (
-                            <ListItem key={file.meta.path}>{file.meta.name}</ListItem>
-                        ))}
-                    </List>
-                    <Button
-                        type='reset' size='small' variant='outlined' color='error'
-                        style={{marginBottom: '1rem'}}
-                        onClick={(event) => {
-                            event.preventDefault();
-                            setTestFiles([]);
-                        }}
-                    >
-                        Сбросить все тесты
-                    </Button>
-                    <Button
-                        type='submit' size='medium' variant='contained' color='primary'
-                        onClick={handleSubmit}
-                    >
-                        Запустить
-                    </Button>
-                </FormControl>
-                <Box>
-                    <List>
-                        {logs.map(log => (
-                            <ListItem key={testIndex++}>{log}</ListItem>
-                        ))}
-                    </List>
-                </Box>
-            </Box>)}
-            {activeTab === 'result' && (<Box>
-                
-            </Box>)}
+            {activeTab === 'upload' && <UploadPanel hostname={hostname} />}
+            {activeTab === 'result' && (<ResultPanel hostname={hostname} />)}
         </Box>
     </>);
 }
---
+++ v1/frontend/src/panels/result.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import { Button, FormControl, FormLabel, TextField } from '@mui/material';
+
+
+const ResultPanel = ({hostname}) => {
+    const [username, setUsername] = useState('');
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        window.location.assign(`http://${hostname}/getvideo/${username}`);
+    };
+
+    return (
+        <FormControl
+            action='#'
+            method='POST'
+            encType='multipart/form-data'
+            style={{
+                width: '100%',
+                margin: '1rem 0',
+            }}
+        >
+            <FormLabel> Пользователь </FormLabel>
+            <TextField
+                placeholder='username' type='text' size='medium' variant='outlined'
+                value={username}
+                onChange={event => setUsername(event.target.value)}
+            />
+            <Button
+                type='button' size='medium' variant='contained' color='primary'
+                onClick={handleSubmit}
+                style={{marginTop: '1rem'}}
+            >
+                Получить результаты
+            </Button>
+        </FormControl>
+    );
+};
+
+export default ResultPanel;
